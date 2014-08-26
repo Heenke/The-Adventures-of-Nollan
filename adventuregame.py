6113b23a4cd469e64e69cc8bbfdf2f053389ba2e
Heenke <henrik.nilson91@gmail.com>
2014-08-26 19:40:16 +0200

    It is working!!
    Walk west or east, try north if you dare. Loot and the go north. Do not
    try south it will only be trouble

--- adventuregame.py
+++ adventuregame.py
@@ -1,3 +1,5 @@
+import time
+
 class room:
 	def __init__(self, name, description, monster_nick, key, x, y):
 		self.name = name
@@ -56,14 +58,14 @@ Tbb = monster('The big Bat', 'Screen', 'Is tha bat blind (y/n)', 'y')
 Wk = monster('Wizard King', 'Computer', '4 + 4', '8')
 
 TDF = room('The Dark Forest', 'The forest is dark, there is a monster guarding the loot, attack the spider for the loot', Sp, '0', '-1', '0')
-CC = room('Concrete Cave', 'Damp', Tbb, '0', '1', '0')
+CC = room('Concrete Cave', 'The caves are deep and narrow, there is a Big Bat guarding the loot', Tbb, '0', '1', '0')
 Start = room('Start', 'Empty, maybe you want to walk east or west or north', 0, '0', '0', '0')
 WP = room('Wizards Palace', 'Classy', Wk, '2', '0', '1')
 
 places = [TDF, CC, Start, WP]
 
 def check_cor_x():
-	global new_cor, y, x, places, arg_cor, newdestination
+	global new_cor, y, x, places, arg_cor, destination, km
 	if new_cor > 1:
 		print 'Cannot go further in this direction'
 	elif new_cor < -1:
@@ -71,11 +73,16 @@ def check_cor_x():
 	else:
 		for i in places:
 			if i.getX() == str(new_cor) and i.getY() == str(y):
-				newdestination = i
-				x = new_cor
+				if len(km) >= int(i.getKey()):
+					destination = i
+					x = new_cor
+				else:
+					print 'Kill more enemies first and get more loot'
+					print 'You need %s' %int(i.getKey()),
+					print 'to go there, check you inventory by typing inv'
 
 def check_cor_y():
-	global new_cor, y, x, places, arg_cor, newdestination, km
+	global new_cor, y, x, places, arg_cor, destination, km
 	if new_cor > 1:
 		print 'Cannot go further in this direction'
 	elif new_cor < -1:
@@ -84,31 +91,29 @@ def check_cor_y():
 		for i in places:
 			if i.getY() == str(new_cor) and i.getX() == str(x):
 				if len(km) >= int(i.getKey()):
-					newdestination = i
+					destination = i
 					y = new_cor
 				else:
-					print 'Kill more enemies first!!!!'
+					print 'Kill more enemies first and get more loot'
+					print 'You need %s' %int(i.getKey()),
+					print 'to go there, check you inventory by typing inv'
 def move():
-	global destination, new_cor, x, y, newdestination
+	global destination, new_cor, x, y
 	arg_cor = str(raw_input('Which Direction? > '))
 	if arg_cor == 'north':
 		new_cor = y + 1
 		check_cor_y()
-		destination = newdestination
 	elif arg_cor == 'south':
 		new_cor = y - 1
 		check_cor_y()
-		destination = newdestination
 	elif arg_cor == 'east':
 		new_cor = x + 1
 		check_cor_x()
-		destination = newdestination
 	elif arg_cor == 'west':
 		new_cor = x - 1
 		check_cor_x()
-		destination = newdestination
 	else:
-		print 'Cannot go to %s' %arg	
+		print 'Cannot go to %s' %arg_cor	
 
 def look():
 	global destination
@@ -120,7 +125,7 @@ def look():
 # Here is the attack function 
 def attack():
 	global destination, hp, inventory, km
-	if destination == 'Start':
+	if destination == Start:
 		print 'Nothing to attack here, you need to WALK somewhere'
 	elif destination.getMonster() in km:
 		print "You have already slained this enemy"
@@ -133,13 +138,16 @@ def attack():
 			inventory.append(att_enemy.getLoot())
 			km.append(destination.getMonster())
 			if len(km) == 2:
-				print 'Travel north to the wizard, he will help you build your computer'
+				print 'Travel north to the Palace and battle the mighty Wizard'
 		else:
 			hp = hp - 2
 			print 'You have lost 2 hp, remaining %s hp' %hp
 def inv():
 	global inventory
-	print inventory
+	if len(inventory) == 0:
+		print 'It is empty...'
+	else:
+		print inventory
 
 def done():
 	global km
@@ -158,8 +166,17 @@ new_cor = 0
 destination = Start
 newdestination = 0
 
+print 'Welcome to The adventures of Nollan'
+time.sleep(1)
+print 'To navigate through the world type walk and then east, west or north'
+time.sleep(1)
+print 'The quest is to collect the loot from The Dark Forest in the west and'
+time.sleep(1)
+print ' the loot from Concrete Caves in the east'
+time.sleep(1)
+print 'When you have all the parts it is time to battle the Wizard in his Palace in the north'
+time.sleep(1)
 while hp > 0:
-	print x, y
 	print 'You are now in',
 	destination.getName()
 	arg = str(raw_input("What is your next move? > "))
